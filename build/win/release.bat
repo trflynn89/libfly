124a44bb0e64b1caa25eddc0551b7e36646a2028
Tim Flynn <trflynn89@gmail.com>
2016-08-18 23:37:21 -0400

    Windows - move build files to build/win

--- build/win/release.bat
+++ build/win/release.bat
@@ -5,7 +5,7 @@ set msbuild=%ProgramFiles(x86)%"\MSBuild\14.0\Bin\MSBuild.exe"
 if not exist "%msbuild%" echo error: "%msbuild%": not found & goto :eof
 
 :: Set the build directory
-set root_dir=%~dp0..
+set root_dir=%~dp0
 if not exist "%root_dir%" echo error: "%root_dir%": not found & goto :eof
 
 :: Perform the build
@@ -15,8 +15,8 @@ if %errorlevel% GEQ 1 echo error: Failed debug x64 build & goto :eof
 "%msbuild%" %root_dir%\libfly.sln /m /t:Rebuild /p:Configuration=Debug /p:Platform=x86
 if %errorlevel% GEQ 1 echo error: Failed debug x86 build & goto :eof
 
-"%msbuild%" %root_dir%\libfly.vcxproj /m /t:Rebuild /p:Configuration=Release /p:Platform=x64
+"%msbuild%" %root_dir%\libfly.sln /m /t:libfly:Rebuild /p:Configuration=Release /p:Platform=x64
 if %errorlevel% GEQ 1 echo error: Failed release x64 build & goto :eof
 
-"%msbuild%" %root_dir%\libfly.vcxproj /m /t:Rebuild /p:Configuration=Release /p:Platform=x86
+"%msbuild%" %root_dir%\libfly.sln /m /t:libfly:Rebuild /p:Configuration=Release /p:Platform=x86
 if %errorlevel% GEQ 1 echo error: Failed release x86 build & goto :eof
