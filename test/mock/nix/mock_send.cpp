2342d175e191d966ce108abd8ef5f40d1183a83c
Tim Flynn <trflynn89@gmail.com>
2018-08-31 23:47:02 -0400

    Make socket test faster by not using huge messages (#10)
    The point of using a really large message was to force packet fragmentation.
    This can be mocked instead. Add a mock system flag for send() to enable mocking
    EWOULDBLOCK. If set, send all but the last byte of the string, setting errno to
    EWOULDBLOCK. On subsequent calls, send the last byte through.
    
    Also fix some bad return value checks in the Window's socket implementation
    (should be checking for SOCKET_ERROR instead of -1).

--- test/mock/nix/mock_send.cpp
+++ test/mock/nix/mock_send.cpp
@@ -19,6 +19,17 @@ extern "C"
             errno = 0;
             return -1;
         }
+        else if (fly::MockSystem::MockEnabled(fly::MockCall::Send_Blocking))
+        {
+            // On the first call, send all but the last byte, setting errno to
+            // indicate that the send would have "blocked". Subsequent calls
+            // will have len == 1; go ahead and finish the send at that point.
+            if (len > 1)
+            {
+                errno = EWOULDBLOCK;
+                len -= 1;
+            }
+        }
 
         return __real_send(sockfd, buf, len, flags);
     }
