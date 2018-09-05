76d5f8dcdf68146d3825f345021b68b19e2135f1
trflynn89 <trflynn89@gmail.com>
2018-09-05 10:54:11 -0400

    Record stdout/stderr and test duration

--- build/win/test.ps1
+++ build/win/test.ps1
@@ -7,17 +7,24 @@ function Run-Libfly-Test($arch)
     $failed = $FALSE
 
     Get-ChildItem -path $full_path -Recurse -Include *.exe  | ForEach {
-        $test = $_.Directory.Name
-        & $_
+        $timer = [Diagnostics.Stopwatch]::StartNew()
+        & $_ 2>&1 | tee -Variable $output
+        $timer.Stop()
+
+        $duration = $timer.Elapsed.TotalMilliseconds
+        $test = $_.Directory.Name + "_" + $arch
+
+        $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
+        $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
 
         if ($LASTEXITCODE)
         {
-            Add-AppveyorTest $test -Outcome Failed -FileName $_ -ErrorMessage "Failed $test test"
+            Add-AppveyorTest $test -Outcome Failed -FileName $_ -StdOut "$stdout.ToString()" -StdErr "$stderr.ToString()" -Duration $duration -ErrorMessage "Failed $test test"
             $failed = $TRUE
         }
         else
         {
-            Add-AppveyorTest $test -Outcome Passed -FileName $_
+            Add-AppveyorTest $test -Outcome Passed -FileName $_ -StdOut "$stdout.ToString()" -StdErr "$stderr.ToString()" -Duration $duration
         }
     }
 
