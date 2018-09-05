fba7eaf46ca2bcbe2716cadbfec710d8f4611d35
trflynn89 <trflynn89@gmail.com>
2018-09-05 11:58:50 -0400

    Clean up

--- build/win/test.ps1
+++ build/win/test.ps1
@@ -9,22 +9,24 @@ function Run-Libfly-Test($arch)
     Get-ChildItem -path $full_path -Recurse -Include *.exe  | ForEach {
         $timer = [Diagnostics.Stopwatch]::StartNew()
         & $_ 2>&1 | tee -Variable output
+        $status = $LASTEXITCODE
         $timer.Stop()
 
         $duration = $timer.Elapsed.TotalMilliseconds
         $test = $_.Directory.Name + "_" + $arch
 
-        if ($LASTEXITCODE)
-        {
-            $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
-            $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
+        $output = Extract-Stdout-Stderr($output)
+        $stdout = $output[0]
+        $stderr = $output[1]
 
-            Add-AppveyorTest $test -Outcome Failed -FileName $_ -StdOut ($stdout | Out-String).Trim() -StdErr ($stderr | Out-String).Trim() -Duration $duration -ErrorMessage "Failed $test test"
+        if ($status)
+        {
+            Add-AppveyorTest $test -Outcome Failed -FileName $_ -StdOut $stdout -StdErr $stderr -Duration $duration -ErrorMessage "Failed $test test"
             $failed = $TRUE
         }
         else
         {
-            Add-AppveyorTest $test -Outcome Passed -FileName $_ -StdOut ($output | Out-String).Trim() -Duration $duration
+            Add-AppveyorTest $test -Outcome Passed -FileName $_ -StdOut $stdout -StdErr $stderr -Duration $duration
         }
     }
 
@@ -33,6 +35,33 @@ function Run-Libfly-Test($arch)
         Write-Error "Failed $arch tests"
         exit 1
     }
+}
+
+function Extract-Stdout-Stderr($output)
+{
+    $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
+    $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
+
+    if ([string]::IsNullOrEmpty($stdout))
+    {
+        $stdout = (Out-String).Trim()
+    }
+    else
+    {
+        $stdout = ($stdout | Out-String).Trim()
+    }
+
+    if ([string]::IsNullOrEmpty($stderr))
+    {
+        $stderr = (Out-String).Trim()
+    }
+    else
+    {
+        $stderr = ($stderr | Out-String).Trim()
+    }
+
+    $stdout
+    $stderr
 }
 
 # Run the tests
