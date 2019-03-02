956af184b4b0daefadb5d8c2339e544ffcb8f6fc
Timothy Flynn <trflynn89@pm.me>
2019-03-01 23:22:41 -0500

    Maybe fix tests

--- build/win/test.ps1
+++ build/win/test.ps1
@@ -9,25 +9,19 @@ function Run-Libfly-Test($arch)
 
     Get-ChildItem -path $full_path -Recurse -Include *_tests.exe  | ForEach {
         $timer = [Diagnostics.Stopwatch]::StartNew()
-        & $_ 2>&1 | tee -Variable output
+        & $_
         $status = $LASTEXITCODE
         $timer.Stop()
 
         $duration = $timer.Elapsed.TotalMilliseconds
         $test = $_.Directory.Name + "_" + $arch
 
-        $output = Extract-Stdout-Stderr($output)
-        $stdout = $output[0]
-        $stderr = $output[1]
-
         if ($status -eq 0)
         {
-            # Add-AppveyorTest $test -Outcome Passed -FileName $_ -StdOut $stdout -StdErr $stderr -Duration $duration
             ++$tests_passed
         }
         else
         {
-            # Add-AppveyorTest $test -Outcome Failed -FileName $_ -StdOut $stdout -StdErr $stderr -Duration $duration -ErrorMessage "Failed $test test"
             ++$tests_failed
         }
     }
@@ -37,33 +31,6 @@ function Run-Libfly-Test($arch)
         Write-Error "Failed $arch tests"
         exit 1
     }
-}
-
-function Extract-Stdout-Stderr($output)
-{
-    $stdout = $output | ?{ $_ -isnot [System.Management.Automation.ErrorRecord] }
-    $stderr = $output | ?{ $_ -is [System.Management.Automation.ErrorRecord] }
-
-    if ([string]::IsNullOrEmpty($stdout))
-    {
-        $stdout = (Out-String).Trim()
-    }
-    else
-    {
-        $stdout = ($stdout | Out-String).Trim()
-    }
-
-    if ([string]::IsNullOrEmpty($stderr))
-    {
-        $stderr = (Out-String).Trim()
-    }
-    else
-    {
-        $stderr = ($stderr | Out-String).Trim()
-    }
-
-    $stdout
-    $stderr
 }
 
 # Run the tests
