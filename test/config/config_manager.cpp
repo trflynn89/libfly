c042a52ebed9878af60a814ce67813630e7ffbb6
Timothy Flynn <trflynn89@pm.me>
2019-09-20 01:44:44 -0400

    Test is flaky on Linux too apparently

--- test/config/config_manager.cpp
+++ test/config/config_manager.cpp
@@ -1,7 +1,6 @@
 #include "fly/config/config_manager.h"
 
 #include "fly/config/config.h"
-#include "fly/fly.h"
 #include "fly/literals.h"
 #include "fly/path/path_config.h"
 #include "fly/task/task_manager.h"
@@ -279,15 +278,13 @@ TEST_F(ConfigManagerTest, FileChangeTest)
     ASSERT_TRUE(fly::PathUtil::WriteFile(m_file, contents2));
     m_spTaskRunner->WaitForTaskTypeToComplete<fly::ConfigUpdateTask>();
 
-#if defined(FLY_WINDOWS)
-    // On Windows, multiple FILE_ACTION_MODIFIED events may be triggered even
-    // though only a single write occurs. If needed, wait for a second event.
-    // https://stackoverflow.com/a/1765094
+    // Multiple fly::PathMonitor::PathEvent::Changed events may be triggered
+    // even though the above write happens as a single call. If needed, wait for
+    // a second event.
     if (spConfig->GetValue<std::string>("name", "").empty())
     {
         m_spTaskRunner->WaitForTaskTypeToComplete<fly::ConfigUpdateTask>();
     }
-#endif
 
     EXPECT_EQ(spConfig->GetValue<std::string>("name", ""), "Jane Doe");
     EXPECT_EQ(spConfig->GetValue<std::string>("address", ""), "");
