dd68f8fa6403a1d6f467de95859a57c0f101345c
Timothy Flynn <trflynn89@pm.me>
2019-11-18 20:25:25 -0500

    A better qt_widget example

---
+++ examples/qt/qt_widget/notepad.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QtCore/QString>
+#include <QtWidgets/QMainWindow>
+
+#include <memory>
+
+namespace Ui {
+class Notepad;
+} // namespace Ui
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT;
+
+public:
+    explicit Notepad(QWidget *parent = nullptr);
+    ~Notepad();
+
+private slots:
+    void newDocument();
+    void open();
+    void save();
+    void saveAs();
+    void print();
+    void exit();
+    void copy();
+    void cut();
+    void paste();
+    void undo();
+    void redo();
+    void selectFont();
+    void setFontBold(bool bold);
+    void setFontUnderline(bool underline);
+    void setFontItalic(bool italic);
+    void about();
+
+private:
+    std::unique_ptr<Ui::Notepad> m_upUi;
+    QString m_currentFile;
+};
