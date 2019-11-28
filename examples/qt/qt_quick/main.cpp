527ab877bfb68cbed95f6ef7d7c94c23886f3411
Tim Flynn <trflynn89@pm.me>
2019-11-28 12:18:57 -0500

    Add READMEs for examples (#66)

--- examples/qt/qt_quick/main.cpp
+++ examples/qt/qt_quick/main.cpp
@@ -1,19 +1,7 @@
 #include <QtGui/QGuiApplication>
 #include <QtQml/QQmlApplicationEngine>
 
-#if defined(_WIN32)
-// Requires "Qt Visual Studio Tools" extension:
-// https://marketplace.visualstudio.com/items?itemName=TheQtCompany.QtVisualStudioTools2019
-//
-// Once installed, in VS, open Extensions > Qt VS Tools > Qt Options. Add both
-// the installation:
-//      Name: Qt_5.13.2, Path: C:\Qt\5.13.2\msvc2017_64
-//
-// In new Qt projects, change the following project properties:
-//      Qt Project Settings > Qt Installation = Qt_5.13.2_$(PlatformTarget)
-//      Qt Project Settings > Qt Modules = core;gui;qml
-//      Qt Resource Compiler > Output File Name = %(Filename).rcc.cpp
-#elif defined(__linux__)
+#if defined(__linux__)
 extern "C"
 {
     // AddressSanitizer reports leaks from some system libraries. Override the
