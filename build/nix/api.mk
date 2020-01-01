5448ef746ea625bcec243fe62f1f2a006ee1762b
Tim Flynn <trflynn89@pm.me>
2020-01-01 18:29:59 -0500

    Add API for excluding paths from code coverage (#81)
    Use this API rather than hard-coding paths to exclude in build.mk.
    Continue always excluding /usr for gcc, though.

--- build/nix/api.mk
+++ build/nix/api.mk
@@ -1,13 +1,29 @@
 # Set of API functions for using the build system. Applications using this build
 # system should include this file first.
 
+# Verify expected variables
+ifeq ($(SOURCE_ROOT),)
+    $(error SOURCE_ROOT must be defined)
+else ifeq ($(wildcard $(SOURCE_ROOT)/.*),)
+    $(error SOURCE_ROOT $(SOURCE_ROOT) does not exist)
+endif
+
+ifeq ($(VERSION),)
+    $(error VERSION must be defined)
+else ifneq ($(words $(subst ., ,$(VERSION))), 3)
+    $(error VERSION $(VERSION) must be of the form X.Y.Z)
+endif
+
 # List of all target names
 TARGETS :=
 
 # List of all test target names
 TEST_TARGETS :=
 
-# Function to define a target.
+# List of paths to ignore during code coverage reporting
+COVERAGE_BLACKLIST :=
+
+# Define a target.
 #
 # $(1) = The target's name.
 # $(2) = The target's source directory.
@@ -23,5 +39,18 @@ ifeq ($(strip $(3)), TEST)
 else
     TARGET_TYPE_$$(strip $(1)) := $(3)
 endif
+
+endef
+
+# Add a path the the code coverage blacklist.
+#
+# $(1) = The path to ignore.
+define IGNORE_FOR_COVERAGE
+
+ifeq ($$(wildcard $(SOURCE_ROOT)/$$(strip $(1))),)
+    $$(error Could not find path $$(strip $(1)), check your Makefile)
+endif
+
+COVERAGE_BLACKLIST += $(1)
 
 endef
