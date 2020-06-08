ff70184d309e9f6c5edd06ee4df74912e4743564
Timothy Flynn <trflynn89@pm.me>
2020-06-08 14:48:50 -0400

    Missed new files

---
+++ fly/parser/parser_exception.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <cstdint>
+#include <exception>
+#include <string>
+
+namespace fly {
+
+/**
+ * Exception to be raised if an error was encountered while parsing.
+ *
+ * @author Timothy Flynn (trflynn89@pm.me)
+ * @version April 21, 2018
+ */
+class ParserException : public std::exception
+{
+public:
+    /**
+     * Constructor.
+     *
+     * @param line Line number where error was encountered.
+     * @param message Message indicating what error was encountered.
+     */
+    ParserException(std::uint32_t line, const std::string &message) noexcept;
+
+    /**
+     * Constructor.
+     *
+     * @param line Line number where error was encountered.
+     * @param column Column number in line where error was encountered.
+     * @param message Message indicating what error was encountered.
+     */
+    ParserException(std::uint32_t line, std::uint32_t column, const std::string &message) noexcept;
+
+    /**
+     * @return A C-string representing this exception.
+     */
+    const char *what() const noexcept override;
+
+private:
+    const std::string m_message;
+};
+
+/**
+ * Exception to be raised if an unexpected character was encountered.
+ *
+ * @author Timothy Flynn (trflynn89@pm.me)
+ * @version April 21, 2018
+ */
+class UnexpectedCharacterException : public ParserException
+{
+public:
+    /**
+     * Constructor.
+     *
+     * @param line Line number where error was encountered.
+     * @param column Column number in line where error was encountered.
+     * @param ch Unexpected character code.
+     */
+    UnexpectedCharacterException(std::uint32_t line, std::uint32_t column, int ch) noexcept;
+};
+
+/**
+ * Exception to be raised if a value could not be converted as expected.
+ *
+ * @author Timothy Flynn (trflynn89@pm.me)
+ * @version April 21, 2018
+ */
+class BadConversionException : public ParserException
+{
+public:
+    /**
+     * Constructor.
+     *
+     * @param line Line number where error was encountered.
+     * @param column Column number in line where error was encountered.
+     * @param value The unconvertable value.
+     */
+    BadConversionException(
+        std::uint32_t line,
+        std::uint32_t column,
+        const std::string &value) noexcept;
+};
+
+} // namespace fly
