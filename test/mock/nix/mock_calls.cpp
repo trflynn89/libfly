dad342148a43fd67bc4501cace39ce5fa57e8064
Timothy Flynn <trflynn89@pm.me>
2020-07-11 10:29:11 -0400

    Add MockCall for ::isatty()
    And remove operator<<(MockCall) as it is no longer used.

--- test/mock/nix/mock_calls.cpp
+++ test/mock/nix/mock_calls.cpp
@@ -45,92 +45,6 @@ int s_sendto_call_count = 0;
 
 } // namespace
 
-namespace fly {
-
-//==================================================================================================
-std::ostream &operator<<(std::ostream &stream, MockCall call)
-{
-    switch (call)
-    {
-        case MockCall::Accept:
-            stream << "accept";
-            break;
-        case MockCall::Backtrace:
-            stream << "backtrace";
-            break;
-        case MockCall::BacktraceSymbols:
-            stream << "backtrace_symbols";
-            break;
-        case MockCall::Bind:
-            stream << "bind";
-            break;
-        case MockCall::Connect:
-            stream << "connect";
-            break;
-        case MockCall::Fcntl:
-            stream << "fcntl";
-            break;
-        case MockCall::Gethostbyname:
-            stream << "gethostbyname";
-            break;
-        case MockCall::Getsockopt:
-            stream << "getsockopt";
-            break;
-        case MockCall::InotifyAddWatch:
-            stream << "inotify_add_watch";
-            break;
-        case MockCall::InotifyInit1:
-            stream << "inotify_init1";
-            break;
-        case MockCall::Listen:
-            stream << "listen";
-            break;
-        case MockCall::LocalTime:
-            stream << "localtime";
-            break;
-        case MockCall::Poll:
-            stream << "poll";
-            break;
-        case MockCall::Read:
-            stream << "read";
-            break;
-        case MockCall::Recv:
-            stream << "recv";
-            break;
-        case MockCall::Recvfrom:
-            stream << "recvfrom";
-            break;
-        case MockCall::Send:
-            stream << "send";
-            break;
-        case MockCall::SendBlocking:
-            stream << "send (blocking)";
-            break;
-        case MockCall::Sendto:
-            stream << "sendto";
-            break;
-        case MockCall::SendtoBlocking:
-            stream << "sendto (blocking)";
-            break;
-        case MockCall::Setsockopt:
-            stream << "setsockopt";
-            break;
-        case MockCall::Socket:
-            stream << "socket";
-            break;
-        case MockCall::Sysinfo:
-            stream << "sysinfo";
-            break;
-        case MockCall::Times:
-            stream << "times";
-            break;
-    }
-
-    return stream;
-}
-
-} // namespace fly
-
 #ifdef __cplusplus
 extern "C"
 {
@@ -287,6 +201,22 @@ extern "C"
         }
 
         return __real_inotify_init1(flags);
+    }
+
+    //==============================================================================================
+    int __real_isatty(int fd);
+
+    int __wrap_isatty(int fd)
+    {
+        bool fail;
+
+        if (fly::MockSystem::mock_enabled(fly::MockCall::IsATTY, fail))
+        {
+            errno = 0;
+            return fail ? 0 : 1;
+        }
+
+        return __real_isatty(fd);
     }
 
     //==============================================================================================
