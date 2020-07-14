708a620607a8ce202d9d09d46781ed9a8740e459
Timothy Flynn <trflynn89@pm.me>
2020-07-14 17:48:45 -0400

    Instrument test time duration in FlyReporter

--- test/main.cpp
+++ test/main.cpp
@@ -25,12 +25,33 @@ public:
         return "Catch2 test reporter for libfly";
     }
 
+    void testRunStarting(const Catch::TestRunInfo &info) override
+    {
+        Catch::ConsoleReporter::testRunStarting(info);
+        m_test_start = std::chrono::system_clock::now();
+    }
+
+    void testRunEnded(const Catch::TestRunStats &stats) override
+    {
+        Catch::ConsoleReporter::testRunEnded(stats);
+
+        const auto end = std::chrono::system_clock::now();
+        const auto duration = std::chrono::duration<double>(end - m_test_start);
+
+        // ConsoleReporter prints a second newline above, so go up one line before logging the time.
+        stream << fly::Styler(fly::Position::CursorUp, fly::Style::Bold, fly::Color::Cyan)
+               << "Total time ";
+        stream << fly::String::format("%f seconds", duration.count()) << '\n';
+    }
+
     void testCaseStarting(const Catch::TestCaseInfo &info) override
     {
         stream << fly::Styler(fly::Style::Bold, fly::Color::Green)
                << fly::String::format("[==== Test Case: %s ====]", info.name) << '\n';
 
         Catch::ConsoleReporter::testCaseStarting(info);
+
+        m_current_test_case_start = std::chrono::system_clock::now();
         m_current_test_case = info.name;
     }
 
@@ -47,17 +68,24 @@ public:
 
     void testCaseEnded(const Catch::TestCaseStats &stats) override
     {
+        const auto end = std::chrono::system_clock::now();
+        const auto duration = std::chrono::duration<double>(end - m_current_test_case_start);
+
         const std::string &name = stats.testInfo.name;
 
         if (stats.totals.assertions.allOk())
         {
-            stream << fly::Styler(fly::Style::Bold, fly::Color::Green)
-                   << fly::String::format("[==== PASSED %s ====]", name) << "\n\n";
+            stream
+                << fly::Styler(fly::Style::Bold, fly::Color::Green)
+                << fly::String::format("[==== PASSED %s (%f seconds) ====]", name, duration.count())
+                << "\n\n";
         }
         else
         {
-            stream << fly::Styler(fly::Style::Bold, fly::Color::Red)
-                   << fly::String::format("[==== FAILED %s ====]", name) << "\n\n";
+            stream
+                << fly::Styler(fly::Style::Bold, fly::Color::Red)
+                << fly::String::format("[==== FAILED %s (%f seconds) ====]", name, duration.count())
+                << "\n\n";
         }
 
         Catch::ConsoleReporter::testCaseEnded(stats);
@@ -65,6 +93,9 @@ public:
     }
 
 private:
+    std::chrono::system_clock::time_point m_test_start;
+
+    std::chrono::system_clock::time_point m_current_test_case_start;
     std::string m_current_test_case;
 };
 
