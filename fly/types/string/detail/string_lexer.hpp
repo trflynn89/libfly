61e1da8723a6daa922d765bbec2d76b93ec849df
Timothy Flynn <trflynn89@pm.me>
2021-01-06 08:53:07 -0500

    Add string utility for lexical analysis of string literals
    This is a thin wrapper around std::basic_string_view to assist with
    lexical analysis of strings. Provides some basic peeking and consuming
    methods, and ensures the caller cannot move past the end of the string.
    
    This is currently in the fly::detail namespace because it's only use so
    far is a rewrite of the string formatter. It could be promoted up to the
    fly namespace if needed later.

---
+++ fly/types/string/detail/string_lexer.hpp
@@ -0,0 +1,206 @@
+#pragma once
+
+#include "fly/types/string/detail/string_classifier.hpp"
+#include "fly/types/string/detail/string_traits.hpp"
+#include "fly/types/string/string_literal.hpp"
+
+#include <cstdint>
+#include <optional>
+
+namespace fly::detail {
+
+/**
+ * Helper class to perform lexical analysis of a C-string literal. All methods are constant
+ * expressions, allowing for string analysis at compile time.
+ *
+ * @author Timothy Flynn (trflynn89@pm.me)
+ * @version January 3, 2021
+ */
+template <typename StringType>
+class BasicStringLexer
+{
+    using traits = BasicStringTraits<StringType>;
+    using classifier = BasicStringClassifier<StringType>;
+    using char_type = typename traits::char_type;
+    using view_type = typename traits::view_type;
+
+public:
+    using iterator = typename view_type::const_iterator;
+
+    /**
+     * Constructor. Stores a view into a C-string literal. This class is not interested in the null
+     * terminator, thus if provided, it is excluded from the view.
+     *
+     * @tparam N The size of the C-string literal.
+     *
+     * @param literals Reference to a C-string literal of size N.
+     */
+    template <std::size_t N>
+    constexpr explicit BasicStringLexer(const char_type (&literals)[N]) noexcept;
+
+    /**
+     * If a character is available at the current position (or some offset from the current
+     * position) in the C-string literal, return that character.
+     *
+     * @param offset The offset from the current position to peek.
+     *
+     * @return If available, the character at the provided offset. Otherwise, an uninitialized
+     *         value.
+     */
+    constexpr std::optional<char_type> peek(std::size_t offset = 0);
+
+    /**
+     * If a character is available at the current position in the C-string literal, return that
+     * character and advance the current position to the next character.
+     *
+     * @return If available, the current character. Otherwise, an uninitialized value.
+     */
+    constexpr std::optional<char_type> consume();
+
+    /**
+     * If a character is available at the current position in the C-string literal, and if that
+     * character is equivalent to the provided character, advance the current position to the next
+     * character.
+     *
+     * @param ch The character to test with.
+     *
+     * @return Whether the current character was available and matched the provided character.
+     */
+    constexpr bool consume_if(char_type ch);
+
+    /**
+     * Beginning with the current position, retrieve characters from the current position in the
+     * C-string literal and advance the current position to the next character until a character is
+     * either not available or not a decimal digit. Convert the retrieved characters to an unsigned
+     * number.
+     *
+     * @return If consumed, the parsed unsigned number. Otherwise, an uninitialized value.
+     */
+    constexpr std::optional<std::size_t> consume_number();
+
+    /**
+     * @return A pointer to the beginning of the C-string literal.
+     */
+    constexpr iterator begin() const;
+
+    /**
+     * @return A pointer to the end of the C-string literal.
+     */
+    constexpr iterator end() const;
+
+private:
+    /**
+     * If a character is available at the current position in the C-string literal, and if that
+     * character satisfies the provided condition, advance the current position to the next
+     * character.
+     *
+     * @tparam Condition A callable conditional for testing a character.
+     *
+     * @param condition The condition to test with.
+     *
+     * @return Whether the current character was available and satisfied the provided condition.
+     */
+    template <typename Condition>
+    constexpr std::optional<char_type> consume_if(Condition condition);
+
+    static constexpr const auto s_null_terminator = FLY_CHR(char_type, '\0');
+
+    const std::size_t m_size;
+    const view_type m_view;
+
+    std::size_t m_index {0};
+};
+
+//==================================================================================================
+template <typename StringType>
+template <std::size_t N>
+constexpr BasicStringLexer<StringType>::BasicStringLexer(const char_type (&literals)[N]) noexcept :
+    m_size(N - ((literals[N - 1] == s_null_terminator) ? 1 : 0)),
+    m_view(literals, m_size)
+{
+}
+
+//==================================================================================================
+template <typename StringType>
+constexpr auto BasicStringLexer<StringType>::peek(std::size_t offset) -> std::optional<char_type>
+{
+    if ((m_index + offset) >= m_size)
+    {
+        return std::nullopt;
+    }
+
+    return *(m_view.data() + m_index + offset);
+}
+
+//==================================================================================================
+template <typename StringType>
+constexpr auto BasicStringLexer<StringType>::consume() -> std::optional<char_type>
+{
+    if (m_index >= m_size)
+    {
+        return std::nullopt;
+    }
+
+    return *(m_view.data() + m_index++);
+}
+
+//==================================================================================================
+template <typename StringType>
+constexpr bool BasicStringLexer<StringType>::consume_if(char_type ch)
+{
+    if (auto next = peek(); next && (next.value() == ch))
+    {
+        ++m_index;
+        return true;
+    }
+
+    return false;
+}
+
+//==================================================================================================
+template <typename StringType>
+template <typename Condition>
+constexpr auto BasicStringLexer<StringType>::consume_if(Condition condition)
+    -> std::optional<char_type>
+{
+    if (auto next = peek(); next && condition(next.value()))
+    {
+        return consume();
+    }
+
+    return std::nullopt;
+}
+
+//==================================================================================================
+template <typename StringType>
+constexpr std::optional<std::size_t> BasicStringLexer<StringType>::consume_number()
+{
+    bool parsed_number = false;
+    std::size_t number = 0;
+
+    while (auto ch = consume_if(classifier::is_digit))
+    {
+        parsed_number = true;
+
+        number *= 10;
+        number += static_cast<std::size_t>(ch.value() - FLY_CHR(char_type, '0'));
+    }
+
+    return parsed_number ? std::optional<std::size_t>(number) : std::nullopt;
+}
+
+//==================================================================================================
+template <typename StringType>
+constexpr auto BasicStringLexer<StringType>::begin() const -> iterator
+{
+    return m_view.cbegin();
+}
+
+//==================================================================================================
+template <typename StringType>
+constexpr auto BasicStringLexer<StringType>::end() const -> iterator
+{
+    return m_view.cend();
+}
+
+} // namespace fly::detail
---
+++ test/types/string_lexer.cpp
@@ -0,0 +1,258 @@
+#include "fly/types/string/detail/string_lexer.hpp"
+
+#include "fly/types/string/string_literal.hpp"
+
+#include "catch2/catch.hpp"
+
+#include <string>
+
+CATCH_TEMPLATE_TEST_CASE(
+    "BasicStringLexer",
+    "[string]",
+    std::string,
+    std::wstring,
+    std::u8string,
+    std::u16string,
+    std::u32string)
+{
+    using StringType = TestType;
+    using char_type = typename StringType::value_type;
+    using Lexer = fly::detail::BasicStringLexer<StringType>;
+
+    CATCH_SECTION("Cannot consume from empty lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, ""));
+
+        CATCH_CHECK_FALSE(lexer.peek());
+        CATCH_CHECK_FALSE(lexer.consume());
+        CATCH_CHECK_FALSE(lexer.consume_if(FLY_CHR(char_type, '\0')));
+        CATCH_CHECK_FALSE(lexer.consume_number());
+        CATCH_CHECK(lexer.begin() == lexer.end());
+    }
+
+    CATCH_SECTION("Lexer accepts non-null-terminated string")
+    {
+        constexpr const char_type str[] {
+            static_cast<char_type>(0x61), // a
+            static_cast<char_type>(0x62), // b
+        };
+
+        Lexer lexer(str);
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'a')));
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'b')));
+        CATCH_CHECK_FALSE(lexer.consume());
+    }
+
+    CATCH_SECTION("Peeking does not advance internal pointer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto p1 = lexer.peek();
+        CATCH_REQUIRE(p1.has_value());
+        CATCH_CHECK(p1.value() == FLY_CHR(char_type, 'a'));
+
+        auto p2 = lexer.peek();
+        CATCH_REQUIRE(p2.has_value());
+        CATCH_CHECK(p2.value() == FLY_CHR(char_type, 'a'));
+    }
+
+    CATCH_SECTION("Cannot peek past end of lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto p1 = lexer.peek(0);
+        CATCH_REQUIRE(p1.has_value());
+        CATCH_CHECK(p1.value() == FLY_CHR(char_type, 'a'));
+
+        auto p2 = lexer.peek(1);
+        CATCH_REQUIRE(p2.has_value());
+        CATCH_CHECK(p2.value() == FLY_CHR(char_type, 'b'));
+
+        CATCH_CHECK_FALSE(lexer.peek(2));
+    }
+
+    CATCH_SECTION("Consuming character advances internal pointer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto p1 = lexer.peek();
+        CATCH_REQUIRE(p1.has_value());
+        CATCH_CHECK(p1.value() == FLY_CHR(char_type, 'a'));
+
+        auto c1 = lexer.consume();
+        CATCH_REQUIRE(c1.has_value());
+        CATCH_CHECK(c1.value() == FLY_CHR(char_type, 'a'));
+
+        auto p2 = lexer.peek();
+        CATCH_REQUIRE(p2.has_value());
+        CATCH_CHECK(p2.value() == FLY_CHR(char_type, 'b'));
+    }
+
+    CATCH_SECTION("Cannot consume past end of lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto c1 = lexer.consume();
+        CATCH_REQUIRE(c1.has_value());
+        CATCH_CHECK(c1.value() == FLY_CHR(char_type, 'a'));
+
+        auto c2 = lexer.consume();
+        CATCH_REQUIRE(c2.has_value());
+        CATCH_CHECK(c2.value() == FLY_CHR(char_type, 'b'));
+
+        CATCH_CHECK_FALSE(lexer.consume());
+    }
+
+    CATCH_SECTION("Conditional consumption fails if character does not match")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        CATCH_CHECK_FALSE(lexer.consume_if(FLY_CHR(char_type, 'b')));
+
+        auto c1 = lexer.consume();
+        CATCH_REQUIRE(c1.has_value());
+        CATCH_CHECK(c1.value() == FLY_CHR(char_type, 'a'));
+
+        CATCH_CHECK_FALSE(lexer.consume_if(FLY_CHR(char_type, 'a')));
+
+        auto c2 = lexer.consume();
+        CATCH_REQUIRE(c2.has_value());
+        CATCH_CHECK(c2.value() == FLY_CHR(char_type, 'b'));
+    }
+
+    CATCH_SECTION("Conditional consumption advances internal pointer if character matches")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto p1 = lexer.peek();
+        CATCH_REQUIRE(p1.has_value());
+        CATCH_CHECK(p1.value() == FLY_CHR(char_type, 'a'));
+
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'a')));
+
+        auto p2 = lexer.peek();
+        CATCH_REQUIRE(p2.has_value());
+        CATCH_CHECK(p2.value() == FLY_CHR(char_type, 'b'));
+    }
+
+    CATCH_SECTION("Cannot conditionally consume past end of lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'a')));
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'b')));
+        CATCH_CHECK_FALSE(lexer.consume_if(FLY_CHR(char_type, '\0')));
+    }
+
+    CATCH_SECTION("Cannot consume number if no number exists")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        CATCH_CHECK_FALSE(lexer.consume_number());
+    }
+
+    CATCH_SECTION("Cannot consume number past end of lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "1"));
+
+        auto n1 = lexer.consume_number();
+        CATCH_REQUIRE(n1.has_value());
+        CATCH_CHECK(n1.value() == 1);
+
+        CATCH_CHECK_FALSE(lexer.consume_number());
+    }
+
+    CATCH_SECTION("Cannot consume number if number exists past internal pointer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab1"));
+
+        CATCH_CHECK_FALSE(lexer.consume_number());
+    }
+
+    CATCH_SECTION("Number consumption stops at first non-digit character")
+    {
+        Lexer lexer(FLY_ARR(char_type, "1ab"));
+
+        auto n1 = lexer.consume_number();
+        CATCH_REQUIRE(n1.has_value());
+        CATCH_CHECK(n1.value() == 1);
+
+        auto p1 = lexer.peek();
+        CATCH_REQUIRE(p1.has_value());
+        CATCH_CHECK(p1.value() == FLY_CHR(char_type, 'a'));
+    }
+
+    CATCH_SECTION("Number consumption stops at end of lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "1"));
+
+        auto n1 = lexer.consume_number();
+        CATCH_REQUIRE(n1.has_value());
+        CATCH_CHECK(n1.value() == 1);
+
+        CATCH_CHECK_FALSE(lexer.peek());
+    }
+
+    CATCH_SECTION("Number consumption consumes all digits in a row")
+    {
+        Lexer lexer(FLY_ARR(char_type, "123"));
+
+        auto n1 = lexer.consume_number();
+        CATCH_REQUIRE(n1.has_value());
+        CATCH_CHECK(n1.value() == 123);
+
+        CATCH_CHECK_FALSE(lexer.peek());
+    }
+
+    CATCH_SECTION("Number consumption consumes all digits in a row")
+    {
+        Lexer lexer(FLY_ARR(char_type, "123"));
+
+        auto n1 = lexer.consume_number();
+        CATCH_REQUIRE(n1.has_value());
+        CATCH_CHECK(n1.value() == 123);
+
+        CATCH_CHECK_FALSE(lexer.peek());
+    }
+
+    CATCH_SECTION("Number consumption can succeed multiple times per lexer if separated")
+    {
+        Lexer lexer(FLY_ARR(char_type, "123a456"));
+
+        auto n1 = lexer.consume_number();
+        CATCH_REQUIRE(n1.has_value());
+        CATCH_CHECK(n1.value() == 123);
+
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'a')));
+
+        auto n2 = lexer.consume_number();
+        CATCH_REQUIRE(n2.has_value());
+        CATCH_CHECK(n2.value() == 456);
+    }
+
+    CATCH_SECTION("Lexer begin always points at the first character in lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto b1 = lexer.begin();
+        CATCH_CHECK(*b1 == FLY_CHR(char_type, 'a'));
+
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'a')));
+
+        auto b2 = lexer.begin();
+        CATCH_CHECK(*b2 == FLY_CHR(char_type, 'a'));
+    }
+
+    CATCH_SECTION("Lexer end always points just past the last character in lexer")
+    {
+        Lexer lexer(FLY_ARR(char_type, "ab"));
+
+        auto e1 = lexer.end() - 1;
+        CATCH_CHECK(*e1 == FLY_CHR(char_type, 'b'));
+
+        CATCH_CHECK(lexer.consume_if(FLY_CHR(char_type, 'a')));
+
+        auto e2 = lexer.end() - 1;
+        CATCH_CHECK(*e2 == FLY_CHR(char_type, 'b'));
+    }
+}
