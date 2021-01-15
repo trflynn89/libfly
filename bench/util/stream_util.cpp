94472fa5c89129b6ded14de9d64ed4917016dcd4
Timothy Flynn <trflynn89@pm.me>
2021-01-15 15:55:23 -0500

    Remove now-duplicated stream utility from benchmarks
    Supporting std::format styled formatting duplicated ScopedStreamModifiers
    (now fly::detail::BasicStreamModifiers). Remove the benchmark version.

--- bench/util/stream_util.cpp
+++ bench/util/stream_util.cpp
@@ -3,29 +3,6 @@
 namespace fly::benchmark {
 
 //==================================================================================================
-ScopedStreamModifiers::ScopedStreamModifiers(std::ostream &stream) :
-    m_stream(stream),
-    m_locale(stream.getloc()),
-    m_flags(stream.flags()),
-    m_precision(stream.precision())
-{
-}
-
-//==================================================================================================
-ScopedStreamModifiers::~ScopedStreamModifiers()
-{
-    m_stream.imbue(m_locale);
-    m_stream.flags(m_flags);
-    m_stream.precision(m_precision);
-}
-
-//==================================================================================================
-void ScopedStreamModifiers::precision(std::streamsize precision)
-{
-    m_stream.precision(precision);
-}
-
-//==================================================================================================
 std::ios::char_type CommaPunctuation::do_thousands_sep() const
 {
     return ',';
