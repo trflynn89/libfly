aebd27278b0cfb5eed323f7368f3acb517a22001
Timothy Flynn <trflynn89@pm.me>
2021-01-30 15:20:16 -0500

    Remove centering facet from benchmark stream utilities
    BasicString::format now supports center alignment for string types, so
    this feature may be used instead of the custom centering facet.

--- bench/util/stream_util.hpp
+++ bench/util/stream_util.hpp
@@ -9,50 +9,6 @@
 namespace fly::benchmark {
 
 /**
- * Helper class to center text within a specified width within a steam.
- *
- * @author Timothy Flynn (trflynn89@pm.me)
- * @version Decmber 12, 2020
- */
-template <typename StringType>
-class Center
-{
-public:
-    /**
-     * Constructor.
-     *
-     * @param width The width to center the text within.
-     * @param value The text to be centered.
-     */
-    Center(std::size_t width, const StringType &value);
-
-    /**
-     * Print the centered text.
-     *
-     * @param stream The stream to print the text into.
-     * @param center The instance holding the centering data.
-     *
-     * @return The same stream object.
-     */
-    friend std::ostream &operator<<(std::ostream &stream, const Center &center)
-    {
-        center.print(stream);
-        return stream;
-    }
-
-private:
-    /**
-     * Print the centered text.
-     *
-     * @param stream The stream to print the text into.
-     */
-    void print(std::ostream &stream) const;
-
-    const std::streamsize m_width;
-    const StringType &m_value;
-};
-
-/**
  * Locale facet to format numbers with comma separators.
  *
  * @author Timothy Flynn (trflynn89@pm.me)
@@ -71,34 +27,5 @@ protected:
      */
     std::string do_grouping() const override;
 };
-
-//==================================================================================================
-template <typename StringType>
-Center<StringType>::Center(std::size_t width, const StringType &value) :
-    m_width(static_cast<std::streamsize>(width)),
-    m_value(value)
-{
-}
-
-//==================================================================================================
-template <typename StringType>
-void Center<StringType>::print(std::ostream &stream) const
-{
-    const std::streamsize length = static_cast<std::streamsize>(m_value.size());
-
-    if (m_width > length)
-    {
-        const std::streamsize left = (m_width + length) / 2;
-
-        fly::detail::BasicStreamModifiers<std::string> scoped_modifiers(stream);
-        stream << std::right;
-        stream << std::setw(left) << m_value;
-        stream << std::setw(m_width - left) << "";
-    }
-    else
-    {
-        stream << m_value;
-    }
-}
 
 } // namespace fly::benchmark
