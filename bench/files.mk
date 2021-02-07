f26db595d68c5b17c7006def9868dbb86fcfb451
Timothy Flynn <trflynn89@pm.me>
2021-02-07 14:45:26 -0500

    Move Catch2 compile flags to separate file
    This allows for dependent binaries to simply include the flags.mk file,
    rather than having to copy compile flags.

--- bench/files.mk
+++ bench/files.mk
@@ -1,3 +1,5 @@
+include $(SOURCE_ROOT)/extern/catchorg/flags.mk
+
 SRC_DIRS_$(d) += \
     bench/coders \
     bench/json \
@@ -7,11 +9,3 @@ SRC_DIRS_$(d) += \
 
 SRC_$(d) := \
     $(d)/main.cpp
-
-# All benchmarks should have Catch2 on the include path.
-CXXFLAGS_$(d) += \
-    -I$(SOURCE_ROOT)/extern/catchorg/Catch2/src \
-    -DCATCH_CONFIG_PREFIX_ALL \
-    -DCATCH_CONFIG_FAST_COMPILE \
-    -DCATCH_CONFIG_ENABLE_OPTIONAL_STRINGMAKER \
-    -Wno-ctor-dtor-privacy
