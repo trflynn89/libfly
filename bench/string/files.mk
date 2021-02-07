c7dfa07379cd9a813c548f38bc6dca1581058c02
Timothy Flynn <trflynn89@pm.me>
2021-02-07 15:05:20 -0500

    Move {fmt} to //extern

--- bench/string/files.mk
+++ bench/string/files.mk
@@ -1,10 +1,4 @@
+include $(SOURCE_ROOT)/extern/fmtlib/flags.mk
+
 SRC_$(d) := \
     $(d)/benchmark_string.cpp
-
-# {fmt} - https://github.com/fmtlib/fmt
-FMT_FLAGS_$(d) := \
-    -I$(d)/extern/fmt/include \
-    -DFMT_HEADER_ONLY
-
-CFLAGS_$(d) += $(FMT_FLAGS_$(d))
-CXXFLAGS_$(d) += $(FMT_FLAGS_$(d))
