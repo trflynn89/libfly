5d24ea5bed3afc6881ebeeef6e1ff1405dcef011
Timothy Flynn <timothy.flynn@viasat.com>
2021-10-07 14:12:10 -0400

    Convert static_assert in fly::literals::numeric_literals to use a concept

--- fly/types/numeric/detail/literal_parser.hpp
+++ fly/types/numeric/detail/literal_parser.hpp
@@ -7,6 +7,29 @@
 namespace fly::detail {
 
 /**
+ * Concept that is satisfied if the provided character is a valid literal for the provided base.
+ */
+template <std::size_t Base, char Literal>
+concept ValidLiteralForBase = (Literal == '\'') ||
+    ((Base == 2) && ((Literal >= '0') && (Literal <= '1'))) ||
+    ((Base == 8) && ((Literal >= '0') && (Literal <= '7'))) ||
+    ((Base == 10) && ((Literal >= '0') && (Literal <= '9'))) ||
+    ((Base == 16) && ((Literal >= '0') && (Literal <= '9'))) ||
+    ((Base == 16) && ((Literal >= 'A') && (Literal <= 'F'))) ||
+    ((Base == 16) && ((Literal >= 'a') && (Literal <= 'f')));
+
+/**
+ * Concept that is satisfied if the provided value fits within the limits of the desired type to be
+ * converted to.
+ */
+template <typename From, typename To, From Value>
+concept ValueWithinLimits = requires
+{
+    requires(Value >= std::numeric_limits<To>::min());
+    requires(Value <= std::numeric_limits<To>::max());
+};
+
+/**
  * Structures to recursively aggregate the value of an integer literal as it is being parsed.
  *
  * @tparam T The desired integer literal type.
@@ -14,7 +37,11 @@ namespace fly::detail {
  * @tparam Literals Variadic list of characters to parse.
  */
 template <typename T, T Base, char... Literals>
+// clang-format off
+// Formatter disabled because it doesn't handle this constraint well.
+requires(ValidLiteralForBase<Base, Literals> && ...)
 struct Aggregator;
+// clang-format on
 
 // Specialization to convert the current character being parsed to an integer and recurse on the
 // remainder of the characters.
@@ -36,15 +63,6 @@ private:
      */
     static constexpr T parse_and_validate_literal()
     {
-        constexpr bool literal_is_valid_for_base =
-            ((Base == 2) && ((Digit >= '0') && (Digit <= '1'))) ||
-            ((Base == 8) && ((Digit >= '0') && (Digit <= '7'))) ||
-            ((Base == 10) && ((Digit >= '0') && (Digit <= '9'))) ||
-            ((Base == 16) && ((Digit >= '0') && (Digit <= '9'))) ||
-            ((Base == 16) && ((Digit >= 'A') && (Digit <= 'F'))) ||
-            ((Base == 16) && ((Digit >= 'a') && (Digit <= 'f')));
-        static_assert(literal_is_valid_for_base, "Invalid literal for base");
-
         if constexpr ((Digit >= 'A') && (Digit <= 'F'))
         {
             return static_cast<T>(Digit) - static_cast<T>('A') + 0xA;
@@ -137,7 +155,7 @@ struct Parser<T, '0', 'X', Literals...> : ParserBase<T, 16, Literals...>
 };
 
 /**
- * Validate that a value can be converted to a desired type and perform that conversion
+ * Validate that a value can be converted to a desired type and perform that conversion.
  *
  * @tparam From The current integer literal type.
  * @tparam To The desired integer literal type.
@@ -146,11 +164,9 @@ struct Parser<T, '0', 'X', Literals...> : ParserBase<T, 16, Literals...>
  * @return The converted integer literal.
  */
 template <typename From, typename To, From Value>
+requires ValueWithinLimits<From, To, Value>
 constexpr To validate_and_convert()
 {
-    static_assert(Value >= std::numeric_limits<To>::min(), "Literal overflow");
-    static_assert(Value <= std::numeric_limits<To>::max(), "Literal overflow");
-
     return static_cast<To>(Value);
 }
 
