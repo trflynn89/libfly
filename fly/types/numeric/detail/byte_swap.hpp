f11b09befa5e50fe11c6cd2725cc39a46fc60a77
Timothy Flynn <trflynn89@pm.me>
2021-10-08 09:46:03 -0400

    Use builtin byte-swap on Windows when evaulated in non-constant contexts

--- fly/types/numeric/detail/byte_swap.hpp
+++ fly/types/numeric/detail/byte_swap.hpp
@@ -10,6 +10,9 @@
 #    include <libkern/OSByteOrder.h>
 #elif defined(FLY_WINDOWS)
 #    include "fly/types/numeric/literals.hpp"
+
+#    include <cstdlib>
+#    include <type_traits>
 #else
 #    error Unknown byte swapping includes.
 #endif
@@ -52,34 +55,62 @@ constexpr std::uint64_t byte_swap(std::uint64_t value)
 
 #elif defined(FLY_WINDOWS)
 
-// Windows has _byteswap_ushort, _byteswap_ulong, and _byteswap_uint64, but they are non-constexpr.
-// So to allow endian swapping to be used at compile time, use custom byte swapping methods.
+// Built-in byte swapping methods on Windows are non-constexpr. To enable invoking byte swapping at
+// compile time, use shift-based byte swapping methods when evaluated in a constant context. See:
+//
+// https://developercommunity.visualstudio.com/t/constexpr-byte-swapping-optimization/983963
+// https://developercommunity.visualstudio.com/t/Mark-some-built-in-functions-as-constexp/362558?entry=myfeedback&refTime=1620723793774&refUserId=f9653689-1bb1-4374-8703-3fb61971d3f5
 
 constexpr std::uint16_t byte_swap(std::uint16_t value)
 {
-    using namespace fly::literals::numeric_literals;
+    if (std::is_constant_evaluated())
+    {
+        using namespace fly::literals::numeric_literals;
 
-    return ((value & 0xff00_u16) >> 8) | ((value & 0x00ff_u16) << 8);
+        return ((value & 0xff00_u16) >> 8) | ((value & 0x00ff_u16) << 8);
+    }
+    else
+    {
+        return _byteswap_ushort(value);
+    }
 }
 
 constexpr std::uint32_t byte_swap(std::uint32_t value)
 {
-    using namespace fly::literals::numeric_literals;
+    if (std::is_constant_evaluated())
+    {
+        using namespace fly::literals::numeric_literals;
 
-    return (
-        ((value & 0xff00'0000_u32) >> 24) | ((value & 0x00ff'0000_u32) >> 8) |
-        ((value & 0x0000'ff00_u32) << 8) | ((value & 0x0000'00ff_u32) << 24));
+        return (
+            ((value & 0xff00'0000_u32) >> 24) | ((value & 0x00ff'0000_u32) >> 8) |
+            ((value & 0x0000'ff00_u32) << 8) | ((value & 0x0000'00ff_u32) << 24));
+    }
+    else
+    {
+        return _byteswap_ulong(value);
+    }
 }
 
 constexpr std::uint64_t byte_swap(std::uint64_t value)
 {
-    using namespace fly::literals::numeric_literals;
+    if (std::is_constant_evaluated())
+    {
+        using namespace fly::literals::numeric_literals;
 
-    return (
-        ((value & 0xff00'0000'0000'0000_u64) >> 56) | ((value & 0x00ff'0000'0000'0000_u64) >> 40) |
-        ((value & 0x0000'ff00'0000'0000_u64) >> 24) | ((value & 0x0000'00ff'0000'0000_u64) >> 8) |
-        ((value & 0x0000'0000'ff00'0000_u64) << 8) | ((value & 0x0000'0000'00ff'0000_u64) << 24) |
-        ((value & 0x0000'0000'0000'ff00_u64) << 40) | ((value & 0x0000'0000'0000'00ff_u64) << 56));
+        return (
+            ((value & 0xff00'0000'0000'0000_u64) >> 56) |
+            ((value & 0x00ff'0000'0000'0000_u64) >> 40) |
+            ((value & 0x0000'ff00'0000'0000_u64) >> 24) |
+            ((value & 0x0000'00ff'0000'0000_u64) >> 8) |
+            ((value & 0x0000'0000'ff00'0000_u64) << 8) |
+            ((value & 0x0000'0000'00ff'0000_u64) << 24) |
+            ((value & 0x0000'0000'0000'ff00_u64) << 40) |
+            ((value & 0x0000'0000'0000'00ff_u64) << 56));
+    }
+    else
+    {
+        return _byteswap_uint64(value);
+    }
 }
 
 #else
